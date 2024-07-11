5f5775a815aa5a5f4b924c5f299f130237286b65
vha11 <vrbodriguez11@gmail.com>
2024-07-11 14:42:06 -0600

    first commit

---
+++ src/main/java/challenge/dos/alura/appLibreriaDigital/AppLibreriaDigitalApplication.java
@@ -0,0 +1,13 @@
+package challenge.dos.alura.appLibreriaDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLibreriaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLibreriaDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/challenge/dos/alura/appLibreriaDigital/AppLibreriaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.dos.alura.appLibreriaDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLibreriaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
